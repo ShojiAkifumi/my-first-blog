4e7b1f9da09c2df9c54eec2ed3946e5e69cec42c
ShojiAkifumi <kuonnrh48951@outlook.jp>
2020-04-02 19:09:24 +0900

    test

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,5 @@ from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = ["勇者", "魔法使い", "戦士"]
     return render(request, 'blog/post_list.html', {'posts':posts})
